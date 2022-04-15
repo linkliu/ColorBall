d16fbfef89b141d4cb079c85bc2f8ad4d18b5b31
LINKLIU <mucylulu@163.com>
2022-04-15 18:13:42 +0800

    项目初始化

---
+++ ColorBall/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorBall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
